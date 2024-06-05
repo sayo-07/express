deed753b09bf019cc80309f827c7494f7eddc1ef
sayo-07 <ayayon.1207@gmail.com>
2024-06-05 17:19:53 +0900

    change error

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,6 @@
 var express = require('express');
  var router = express.Router();
 
-<<<<<<<  HEAD
 
 
  // 接続情報を設定
@@ -22,34 +21,3 @@ const query = { id: 2 };
  })
 
  module.exports = router;
-=======
- // レスポンスのデータ（ノート全件）
-const responseObjectDataAll = {
- textObject1 : {
- id: 1,
- title: 'ノート１のタイトルです',
- subTitle: 'ノート１のサブタイトルです',
- bodyText: 'ノート１の本文です'
- },
- textObject2 : {
- id: 2,
- title: 'ノート２のタイトルです',
- subTitle: 'ノート２のサブタイトルです',
- bodyText: 'ノート２の本文です'
- },
- };
-
- /**
- * メモを全件取得するAPI
- * @returns {Object[]} data
- * @returns {number} data.id - ID
- * @returns {string} data.title - タイトル
-* @returns {string} data.text - 内容
-*/
- router.get('/', function (req, res, next) {
- // 全件取得して返す
-res.json(responseObjectDataAll);
- })
- 
- module.exports = router;
->>>>>>> 89119b379e4c7b19ebb31fbe126bf2054775dfad
